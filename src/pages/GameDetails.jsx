6f7451d668733681e41eec6ba0d5016ec6940c6e
hugosimon369 <hugosimonalv@gmail.com>
2025-11-25 21:58:17 -0400

    v 0.5.1 - GameDetails completo en funcionalidad JSX. Inicio de estrucutra HTML.

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -10,21 +10,56 @@ function GameDetails() {
 
     const parametros = useParams()
     console.log(parametros)
-
+    console.log(API_KEY)
 
     const [gameDetails, setGameDetails] = useState()
 
+    const [loadingDetails, setLoadingDetails] = useState(false)
+
+    const [errorDetails, setErrorDetails] = useState('')
+
+
     // EFECTOS
-    useEffect(()=>{
-        
-    }, [])
+
+
+    useEffect(() => {
+        setLoadingDetails(true)
+        const id = `${parametros.id}`
+        const url = `https://api.rawg.io/api/games/${id}?key=${API_KEY}`
+        console.log(url)
+        const obtenerJuegos = async () => {
+            try {
+                const respuesta = await fetch(url)
+                const data = await respuesta.json()
+                console.log("¡Detalles recibidos!", data)
+                setGameDetails(data)
+                setLoadingDetails(false)
+                setErrorDetails('')
+            }
+            catch (error) {
+                console.error("Error en la respuesta del Api:", error);
+                setLoadingDetails(false)
+                setErrorDetails(`Error en la respuesta del Api: ${error.message}`)
+            }
+        }
+        obtenerJuegos()
+    }, [parametros])
 
 
     return (
         <div>
             <param name="game" value={parametros.id} />
-            <h2>Detalles del Juego</h2>
-            <p>Aquí veremos la info completa...</p>
+            {errorDetails && <p>{errorDetails}</p>}
+            {loadingDetails && <p>Cargando detalles del juego</p>}
+            {gameDetails &&
+                <>
+                    <h2>{gameDetails.name}</h2>
+                    <img src={gameDetails.backgrond_image} alt="" title={gameDetails.name} />
+                    <p>{gameDetails.description_raw}</p>
+                    <p>{gameDetails.released}</p>
+                    <a href={gameDetails.website} target="_blanck">sitio web</a>
+                </>
+            }
         </div>
     )
 }
